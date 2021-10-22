e0e729403426886bb78b4924629c3d6a82fdad68
Felix Quehl <felix.quehl@wsa.com>
2021-10-22 18:41:33 +0200

    fixed error message

--- sources/main.c
+++ sources/main.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 
     if (argc != 3)
     {
-        printf("port argument missing");
+        printf("host and port argument missing");
         return 1;
     }
     host = argv[1];
